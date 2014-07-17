606b4bf0aa9d77b69aacaee9f20e4706e780f82e
Ryannnnnnn <ryannx6@gmail.com>
2014-07-17 15:37:17 +0800

    Console & .gitignore added

--- node-api-examples/Console.js
+++ node-api-examples/Console.js
@@ -16,4 +16,30 @@ console.warn("warn %d %d", 2, 3)
 var obj = {
     "name": "im Object"
 }
-console.dir(obj)
+console.log(obj)
+console.dir(obj)    // firefox may display a different version than chrome
+                    // when using console.dir, I can figure out the diffs realy
+                    // it use `util.inspect` on obj.
+
+// console.time(label) & console.timeEnd(label)
+console.time("label")
+var a = 0
+for (var i = 0; i < 100000000; i++) {
+    a = a ^ 1
+}
+console.timeEnd("label") // label: 81ms
+
+// console.trace(label)
+console.time("label 2")
+;(function () {
+    var a = 1
+      , b = 2
+      , c
+    c = a + b
+    a = b
+    b = c
+    console.trace("label 2")    // printed to stderr
+}())
+
+// console.assert(expression, [message])
+console.assert(false, "Oh no")  // equivalent to assert.ok
