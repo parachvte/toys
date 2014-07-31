d73e489d99b06f7f957e212158b9c6a57b9da3bc
Ryannnnnnn <ryannx6@gmail.com>
2014-07-31 23:27:52 +0800

    javascript sort string

---
+++ codewars/sort_the_gift_code.js
@@ -0,0 +1,29 @@
+"use strict"
+var assert = require("assert")
+
+function sortGiftCode(code) {
+  var charSet = [], ret = ""
+  for (var i = 0; i < code.length; i++) {
+		if (!(code[i] in charSet)) {
+			charSet.push(code[i])
+		}
+	}
+	charSet.sort()
+	charSet.forEach(function (c) {
+		ret += c
+	})
+	return ret
+}
+
+assert.equal(sortGiftCode("abcdef"), "abcdef")
+assert.equal(sortGiftCode("pqksuvy"), "kpqsuvy")
+assert.equal(sortGiftCode("zyxwvutsrqponmlkjihgfedcba"), "abcdefghijklmnopqrstuvwxyz")
+
+// Solution
+function sortGiftCode2(code) {
+  return code.split("").sort().join("")
+}
+
+assert.equal(sortGiftCode2("abcdef"), "abcdef")
+assert.equal(sortGiftCode2("pqksuvy"), "kpqsuvy")
+assert.equal(sortGiftCode2("zyxwvutsrqponmlkjihgfedcba"), "abcdefghijklmnopqrstuvwxyz")
