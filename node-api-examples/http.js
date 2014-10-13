039a28c6bbd75d84acfd067fd7a3a58f3254e605
Ryannnnnnn <ryannx6@gmail.com>
2014-10-13 14:27:00 +0800

    .jshintrc

--- node-api-examples/http.js
+++ node-api-examples/http.js
@@ -4,3 +4,14 @@
 // http.createServer([RequestListener])
 
 // http.Server
+
+"use strict";
+var http = require("http");
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('okey');
+});
+
+server.listen('127.0.0.1', 3333);
+
